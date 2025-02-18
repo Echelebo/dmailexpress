75c43c3e0ffb840fd492ee712e9e4ed80b27d7a4
Echelebo <echelebochibuike@gmail.com>
2025-02-18 02:05:43 +0100

    f

--- tracking-result.php
+++ tracking-result.php
@@ -127,7 +127,7 @@ extract($data);
         <hr style="margin-top: 20px; border: none;">
         <div class="text-center mt-4">
 
-<h5 class="card-heading-x" style="margin-bottom: 0px;">Sender/Receiver Details</h5>
+<h5 class="card-heading-x" style="margin-bottom: 5px;">Sender/Receiver Details</h5>
 <a href="contact.php" style="color:dodgerblue; ">Contact us now for more information on your package.</a><br /><br />
 </div>
 <div class="text-left">
